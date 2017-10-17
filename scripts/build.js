967789109a40b26d1ae39af4792d710c3f8e8f39
nashaofu <diaocheng@outlook.com>
2017-10-17 22:31:05 +0800

    修改结构

--- scripts/build.js
+++ scripts/build.js
@@ -1,33 +1,33 @@
 const path = require('path')
-const { build, Platform } = require('electron-builder')
+const { build } = require('electron-builder')
 const chalk = require('chalk')
 const { name, author } = require('../package.json')
 
 const config = {
-  targets: Platform.WINDOWS.createTarget(),
-  config: {
-    appId: `com.${author}.${name}`,
-    productName: 'Markdown365',
-    copyright: `Copyright © ${new Date().getFullYear()} ${author}`,
-    asar: true,
-    artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
-    directories: {
-      output: path.join(__dirname, '../release')
-    },
-    linux: {
-      target: 'deb',
-      icon: path.join(__dirname, '../icons'),
-      description: 'Markdown365 —— A powerful markdown editor'
-    },
-    win: {
-      target: 'nsis',
-      icon: path.join(__dirname, '../icons/markdown365.ico')
-    },
-    nsis: {
-      oneClick: false,
-      perMachine: true,
-      allowToChangeInstallationDirectory: true
-    }
+  platform: 'all',
+  appId: `com.${author}.${name}`,
+  productName: 'Markdown365',
+  copyright: `Copyright © ${new Date().getFullYear()} ${author}`,
+  artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
+  asar: true,
+  out: path.join(__dirname, '../release'),
+  overwrite: true,
+  directories: {
+    output: path.join(__dirname, '../release')
+  },
+  linux: {
+    target: 'deb',
+    icon: path.join(__dirname, '../icons'),
+    description: 'Markdown365 —— A powerful markdown editor'
+  },
+  win: {
+    target: 'nsis',
+    icon: path.join(__dirname, '../icons/markdown365.ico')
+  },
+  nsis: {
+    oneClick: false,
+    perMachine: true,
+    allowToChangeInstallationDirectory: true
   }
 }
 
